883df7cbe67eadd33dee0ae4fc0622fcd396b4ac
ulyssesbrabo <ulyssesbrabo@gmail.com>
2017-04-09 12:33:48 -0300

    Excluir funcionando

--- routes.js
+++ routes.js
@@ -30,7 +30,7 @@ module.exports = {
 	configure: function(app){
 		app.all('*', function(req, res, next){
 			res.header("Access-Control-Allow-Origin", '*');
-			res.header("Access-Control-Allow-Methods", '*');
+			res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE, OPTIONS');
 			res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 			next();
 		});
